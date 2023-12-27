05027e58ef3061745b442df86795bd710c7a9e2f
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-12-27 17:43:08 +0100

    Ejercicio mejorado js002

--- Ejercicio1/javascript/js002.js
+++ Ejercicio1/javascript/js002.js
@@ -2,4 +2,12 @@ alert("A continuación, introduzca su nombre");
 
 let userName = prompt("Introuzca su nombre","Escríbalo aquí");
 
+alert(`Tu nombre es ${userName}`);
+
 let mayorEdad = confirm("¿Eres mayo de edad?");
+
+if (mayorEdad === true){
+    alert("Eres mayor de edad.");
+}else {
+    alert("No eres mayor de edad.");
+}
