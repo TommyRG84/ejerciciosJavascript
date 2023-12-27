bdab8f6d945cc1e7724f736664193eaef6accc76
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-12-27 10:48:59 +0100

    Añadido ejericico 01 al repositorio

--- Ejercicio1/javascript/js001.js
+++ Ejercicio1/javascript/js001.js
@@ -1,1 +1,13 @@
-oddsondsonsd
+// Ejercicio 1 
+// Declara dos variables: admin y name.
+// Asigna el valor "John" a name.
+// Copia el valor de name a admin.
+// Muestra el valor de admin usando alert (debe salir “John”).
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
