917fdc1af4fedb0ca68e6e786d02bd4b6b607341
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-12-27 18:35:12 +0100

    Cambios menores

--- Ejercicio1/javascript/js002.js
+++ Ejercicio1/javascript/js002.js
@@ -4,10 +4,11 @@ let userName = prompt("Introuzca su nombre","Escríbalo aquí");
 
 alert(`Tu nombre es ${userName}`);
 
-let mayorEdad = confirm("¿Eres mayo de edad?");
+let mayorEdad = confirm("¿Eres mayor de edad?");
 
 if (mayorEdad === true){
     alert("Eres mayor de edad.");
 }else {
     alert("No eres mayor de edad.");
 }
+
