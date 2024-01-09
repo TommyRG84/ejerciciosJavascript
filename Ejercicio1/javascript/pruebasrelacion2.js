0c3839e872891d4905f81dd7235f66a4b46999e2
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2024-01-09 22:14:16 +0100

    Terminado ejercicio02 de la relacion 02

--- Ejercicio1/javascript/pruebasrelacion2.js
+++ Ejercicio1/javascript/pruebasrelacion2.js
@@ -1,46 +1,32 @@
 /*
-1. Crea un objeto cuyo contenido sea el número ordinal y 
-como valor el número cardinal, es decir, 1: primero, 
-2: segundo… hasta el 10: décimo. 
-Crea una función que cada vez que se introduzca el 
-número ordinal nos dé como resultado el cardinal del número, 
-hasta que se introduzca un número distinto del 1 al 10 
-o cualquier otro valor. Utiliza la instrucción for..in. 
-HAY QUE HACERLO CON OBJETOS.
+2. Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de 
+la suma en binario y en hexadecimal. Utiliza asignación desestructurada.
 */
 
-alert("Ejercicio 01: Crea un objeto cuyo contenido sea el número ordinal y como valor el número cardinal, es decir, 1: primero, 2: segundo… hasta el 10: décimo. Crea una función que cada vez que se introduzca el número ordinal nos dé como resultado el cardinal del número, hasta que se introduzca un número distinto del 1 al 10 o cualquier otro valor. Utiliza la instrucción for..in. HAY QUE HACERLO CON OBJETOS.");
+alert("Ejercicio 02: Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de la suma en binario y en hexadecimal. Utiliza asignación desestructurada.")
 
-let numeros = {
-    1: "primero",
-    2: "segundo",
-    3: "tercero",
-    4: "cuarto",
-    5: "quinto",
-    6: "sexto",
-    7: "séptimo",
-    8: "octavo",
-    9: "noveno",
-    10: "décimo",
+
+let numeroUsuario = Number(prompt("Introduzca un número decimal:"));
+let numeroUsuario2 = Number(prompt("Introduzca un número decimal:"));
+let suma = numeroUsuario + numeroUsuario2;
+
+let { binario, decimal } = {
+    binario: convertirABinario(suma),
+    decimal: suma
 }
 
-let numeroUsuario;
-let salirBucle = false;
+function convertirABinario(suma) {
+    let binario = "";
+    do {
+        binario = (suma % 2) + binario;
+        suma = Math.floor(suma / 2);
 
-do {
-    numeroUsuario = Number(prompt("Introduzca un número:"));
+    } while (suma > 0);
 
-    for (let numero in numeros) {
-        if (numeroUsuario === Number(numero)) {
-            alert(numeros[numeroUsuario]);
-        }
-    }
+    return binario;
+}
 
-    if (numeroUsuario < 1 || numeroUsuario > 10) {
-        salirBucle = true;
-        alert("Número fuera del rango válido.");
-    }
-
-} while (!salirBucle);
+alert(`La suma de los dos números es: ${decimal}`);
+alert(`La suma de los dos números convertido a binario es: ${binario}`);
 
 
--- Ejercicio1/javascript/relacion02.js
+++ Ejercicio1/javascript/relacion02.js
@@ -43,6 +43,32 @@ do {
 
 /*
 2. Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de 
-la suma en binario y en hexadecimal. Utiliza asignación desectructurada.
+la suma en binario y en hexadecimal. Utiliza asignación desestructurada.
 */
+
+alert("Ejercicio 02: Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de la suma en binario y en hexadecimal. Utiliza asignación desestructurada.")
+
+
+numeroUsuario = Number(prompt("Introduzca un número decimal:"));
+let numeroUsuario2 = Number(prompt("Introduzca un número decimal:"));
+let suma = numeroUsuario + numeroUsuario2;
+
+let { binario, decimal } = {
+    binario: convertirABinario(suma),
+    decimal: suma
+}
+
+function convertirABinario(suma) {
+    let binario = "";
+    do {
+        binario = (suma % 2) + binario;
+        suma = Math.floor(suma / 2);
+
+    } while (suma > 0);
+
+    return binario;
+}
+
+alert(`La suma de los dos números es: ${decimal}`);
+alert(`La suma de los dos números convertido a binario es: ${binario}`);
 
