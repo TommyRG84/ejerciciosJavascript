7b87559ec3dbf379c5d818732375edf22d60731a
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2024-01-10 14:23:43 +0100

    Ejercicio03 en proceso

--- Ejercicio1/javascript/pruebasrelacion2.js
+++ Ejercicio1/javascript/pruebasrelacion2.js
@@ -1,32 +1,32 @@
 /*
-2. Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de 
-la suma en binario y en hexadecimal. Utiliza asignación desestructurada.
+3. (OPCIONAL) Lee dos cadenas de caracteres por teclado y visualiza por pantalla lo siguiente:
+(Utiliza la asignación desestructurada)
+NOTA: No puedes utilizar ninguna función predefinida en JS
+a. si la segunda es una subcadena de la primera o no lo es
+b. la cantidad de caracteres de ambas cadenas
+c. devolver la cadena con las vocales en mayúsculas y si la cadena tiene más de 5 caracteres, eliminar desde el quinto hacia adelante y mostrar puntos suspensivos
+Elemplo:
+Entrada 1: “AJjads 2h3iShJasrn aSfd 1f3t f 983d”
+Entrada 2: “ads”
+a. “La segunda cadena es subcadena de la primera”
+b. “La cadena 1 tiene 36 caracteres y la segunda 3 caracteres”
+c. “AJjAd…”
+4. Realiza el ejercicio 3 utilizando alguna función predefinida.
 */
 
-alert("Ejercicio 02: Crea un script que acepte dos valores introducidos en decimal y muestre el resultado de la suma en binario y en hexadecimal. Utiliza asignación desestructurada.")
-
-
-let numeroUsuario = Number(prompt("Introduzca un número decimal:"));
-let numeroUsuario2 = Number(prompt("Introduzca un número decimal:"));
-let suma = numeroUsuario + numeroUsuario2;
-
-let { binario, decimal } = {
-    binario: convertirABinario(suma),
-    decimal: suma
+function pedirCadena(){
+    return prompt("Introduce una cadena de texto:");
+}
+let { cadena1, cadena2 } = {
+    cadena1: pedirCadena(),
+    cadena2: pedirCadena()
 }
 
-function convertirABinario(suma) {
-    let binario = "";
-    do {
-        binario = (suma % 2) + binario;
-        suma = Math.floor(suma / 2);
-
-    } while (suma > 0);
-
-    return binario;
+if (cadena1.indexOf(cadena2) != -1){
+    alert(`${cadena2} está incluida en ${cadena1}`);
+}else{
+    alert(`${cadena2} no está incluida en ${cadena1}`);
 }
 
-alert(`La suma de los dos números es: ${decimal}`);
-alert(`La suma de los dos números convertido a binario es: ${binario}`);
 
 
