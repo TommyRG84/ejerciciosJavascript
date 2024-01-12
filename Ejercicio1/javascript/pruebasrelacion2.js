1fbc63a98212447692b1309dfeae7e238b45830f
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2024-01-12 21:30:21 +0100

    Ejercicio 05 en archivo de pruebas, pendiente integrar con el resto

--- Ejercicio1/javascript/pruebasrelacion2.js
+++ Ejercicio1/javascript/pruebasrelacion2.js
@@ -1,57 +1,34 @@
-/*
-3. (OPCIONAL) Lee dos cadenas de caracteres por teclado y visualiza por pantalla lo siguiente:
-(Utiliza la asignación desestructurada)
-NOTA: No puedes utilizar ninguna función predefinida en JS
-a. si la segunda es una subcadena de la primera o no lo es
-b. la cantidad de caracteres de ambas cadenas
-c. devolver la cadena con las vocales en mayúsculas y si la cadena tiene más de 5 caracteres, eliminar desde el quinto hacia adelante y mostrar puntos suspensivos
-Elemplo:
-Entrada 1: “AJjads 2h3iShJasrn aSfd 1f3t f 983d”
-Entrada 2: “ads”
-a. “La segunda cadena es subcadena de la primera”
-b. “La cadena 1 tiene 36 caracteres y la segunda 3 caracteres”
-c. “AJjAd…”
-4. Realiza el ejercicio 3 utilizando alguna función predefinida.
-*/
+/**
+ * 5. Crea un script que calcule la potencia de un número sin utilizar **, 
+ * pow o cualquier función que realice la tarea directamente. 
+ * La acción debe de ser con números y exponentes positivos. 
+ * Crea una función que realice esta acción y que sea llamada 
+ * cada vez que se introduzcan los valores.
+ */
 
-function pedirCadena(){
-    return prompt("Introduce una cadena de texto:");
-}
-let { cadena1, cadena2 } = {
-    cadena1: pedirCadena(),
-    cadena2: pedirCadena(),
-};
+alert("Ejercicio 05");
 
-if (cadena1.indexOf(cadena2) != -1){
-    alert(`${cadena2} es subcadena de ${cadena1}`);
-}else{
-    alert(`${cadena2} no es subcadena de ${cadena1}`);
-}
+let base;
+let exponente;
 
-alert(`La cadena 1 tiene ${cadena1.length} caracteres y la cadena 2 tiene ${cadena2.length} caracteres.`);
-
-function modificarCadena(inputString){
-
-    let vocalesMinusculas = "aeiou";
-    let cadenaModificada = "";
-    
-    for(let i = 0; i < inputString.length; i++){
-        let letra = inputString[i];
-
-        if(vocalesMinusculas.indexOf(letra) != -1){
-            cadenaModificada += letra.toUpperCase();
-        }else{
-            cadenaModificada += letra;
-        }        
+do{
+    base = Number(prompt("Introduzca un número para obtener la potencia deseada:"));
+    exponente = Number(prompt("Introduzca el exponente a elevar la base introducida:"));
+    if(base < 0 || exponente < 0){
+        alert("Introduzca valores positivos tanto para la base como para el exponente.");
     }
+}while(base < 0 || exponente < 0);
 
-    if(cadenaModificada.length > 5){
-        cadenaModificada = cadenaModificada.slice(0,5) + "...";
-    }
-
-    return cadenaModificada;
+function calcularPotencia(numero1, numero2){
+    let resultado = base;
+    if (exponente === 0){
+        return 1;
+    }else{
+        for(let i = exponente; i > 0 ; i--){
+            resultado = resultado * base;
+        }
+        return resultado;
+    }    
 }
 
-alert(modificarCadena(cadena1));
-alert(modificarCadena(cadena2));
-
+alert(`El resultado de elevar ${base} por ${exponente} es ${calcularPotencia(base, exponente)}`);
